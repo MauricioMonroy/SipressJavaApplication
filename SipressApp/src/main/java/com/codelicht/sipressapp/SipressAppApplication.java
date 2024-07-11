43154a5fb7ea18ab6887a6bf9947f2f438a16686
Mauricio Monroy <mauricio.monroy0@soy.sena.edu.co>
2024-07-11 07:55:07 -0500

    Iniciar proyecto Spring desde cero en la rama version-spring

---
+++ SipressApp/src/main/java/com/codelicht/sipressapp/SipressAppApplication.java
@@ -0,0 +1,13 @@
+package com.codelicht.sipressapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipressAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SipressAppApplication.class, args);
+    }
+
+}
---
+++ SipressApp/src/test/java/com/codelicht/sipressapp/SipressAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelicht.sipressapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipressAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
